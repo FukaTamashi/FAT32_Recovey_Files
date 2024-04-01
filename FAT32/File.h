919d6fc24fed4dc8ab4bdf9a289af11fd5077f01
FukaTamashi <vstepaniuk1@mail.ru>
2024-04-02 00:39:17 +0300

    feat: added header file for File class

---
+++ FAT32/File.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "pch.h"
+#include "BootSector.h"
+
+using namespace std;
+class File
+{
+protected:
+	UINT32 firstCluster;
+	UINT32 size;
+	wstring fileName;
+
+
+public:
+	File() {};
+	~File() {};
+
+	UINT32 getFirstCluster() const;
+	void setFirstCluster(UINT32 startCluster);
+
+	UINT32 getSize() const;
+	void setSize(UINT32 size);
+
+	wstring getFileName() const;
+	void setFileName(wstring& fileName);
+
+	void deleteSpacesInName();
+
+	virtual void createFileName(const UCHAR* record);
+	virtual void createSize(const UCHAR* record);
+	virtual UINT32 createFirstCluster(const UCHAR* record);
+};
