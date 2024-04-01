6829b0b1423d1d1948957fa034ae412c6ca076da
FukaTamashi <vstepaniuk1@mail.ru>
2024-04-02 00:45:41 +0300

    feat: added reader that reads file data

---
+++ FAT32/Reader.cpp
@@ -0,0 +1,47 @@
+#include "pch.h"
+#include "Reader.h"
+
+
+HANDLE Reader::getHandle()
+{
+	return volumeHandle;
+}
+
+bool Reader::OpenDevice(wstring volumeName)
+{
+	if (volumeHandle != INVALID_HANDLE_VALUE) return false;
+	bool returnValue = true;
+	int len = volumeName.length();
+	volumeName.at(len - 1) = 0;
+	volumeHandle = CreateFileW(volumeName.c_str(), GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE,
+		NULL, OPEN_EXISTING, FILE_ATTRIBUTE_DEVICE, NULL);
+	if (volumeHandle == INVALID_HANDLE_VALUE) returnValue = false;
+	return returnValue;
+}
+
+int Reader::ReadSector(UINT64 sector, int sectorSize, int bytesToRead, UCHAR* buffer)
+{
+	if (volumeHandle == INVALID_HANDLE_VALUE) return 0;
+	int result = 0;
+	int remainBytes = bytesToRead % sectorSize;
+	int tempBytesToRead = bytesToRead - remainBytes;
+
+	LARGE_INTEGER shiftInSectors;
+	shiftInSectors.QuadPart = sector * sectorSize;
+	SetFilePointerEx(volumeHandle, shiftInSectors, NULL, FILE_BEGIN);
+
+	DWORD alreadyReadBytes = 0;
+	ReadFile(volumeHandle, buffer, tempBytesToRead, &alreadyReadBytes, NULL);
+	result = alreadyReadBytes;
+
+	if (remainBytes != 0) {
+		UCHAR tempBuf[1024] = {};
+		ReadFile(volumeHandle, tempBuf, sectorSize, &alreadyReadBytes, NULL);
+		// strcpy(buffer + tempBytesToRead, tempBuf);
+		memcpy(buffer + tempBytesToRead, tempBuf, remainBytes);
+		result += remainBytes;
+	}
+
+	return result;
+
+}
