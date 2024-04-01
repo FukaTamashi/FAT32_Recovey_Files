8ee421d40eb91e6c2ff457dc18475b4d27af0a2e
FukaTamashi <vstepaniuk1@mail.ru>
2024-04-02 00:44:33 +0300

    feat: added header file for Menu class

---
+++ FAT32/Menu.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "Volume.h"
+#include "ComputerInfo.h"
+
+class Menu
+{
+public:
+	Menu() {};
+	~Menu() {};
+
+	int chooseVolume(ComputerInfo& info);
+	void printVolumesInformation(ComputerInfo& info) const;
+};
