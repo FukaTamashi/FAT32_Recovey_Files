0083de3540e74b4b9301ddc29d235d07cfefd32a
FukaTamashi <vstepaniuk1@mail.ru>
2024-04-02 00:24:18 +0300

    feat: added implementation for ComputerInfo class

---
+++ FAT32/ComputerInfo.cpp
@@ -0,0 +1,58 @@
+#include "pch.h"
+#include "ComputerInfo.h"
+
+void ComputerInfo::searchVolumes()
+{
+	const int bufferSize = 100;
+	list<Volume> volumes;
+	HANDLE h;
+	LPWSTR volumeName = new WCHAR[bufferSize];
+	h = FindFirstVolumeW(volumeName, bufferSize);
+
+	do {
+
+		Volume volume = Volume(volumeName);
+		volumes.push_back(volume);
+
+	} while (FindNextVolumeW(h, volumeName, bufferSize));
+
+	delete[] volumeName;
+	this->volumes = volumes;
+}
+
+void ComputerInfo::eraseNoNamedVolumes()
+{
+	Volume v;
+	list<Volume>::iterator it;
+	for (it = volumes.begin(); it != volumes.end(); ) {
+		if (it->getLetter()[0] == '*')
+			it = volumes.erase(it);
+		else it++;
+	}
+}
+
+bool ComputerInfo::fileSystemisInComputer(wstring fsName)
+{
+	list<Volume>::iterator it;
+	bool flag = false;
+	for (it = volumes.begin(); it != volumes.end(); it++) {
+		if (it->getFileSystemName() == fsName) {
+			flag = true;
+			break;
+		}
+	}
+	return flag;
+}
+
+Volume ComputerInfo::getVolume(int number)
+{
+	list<Volume>::iterator it = volumes.begin();
+	advance(it, number);
+	return *it;
+}
+
+
+list<Volume> ComputerInfo::getVolumes()
+{
+	return volumes;
+}
