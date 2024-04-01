515182083309547eb6193953ae386b8506323e64
FukaTamashi <vstepaniuk1@mail.ru>
2024-04-02 00:31:11 +0300

    feat: added header file for Conversion

---
+++ FAT32/Conversion.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "pch.h"
+class Conversion
+{
+public:
+	Conversion() {};
+	~Conversion() {};
+
+	UINT32 converseToType(const UCHAR* bytes, int startBorder, int endBorder);
+};
