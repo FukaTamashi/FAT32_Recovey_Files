ecc634fb62e4a15f859c14824bb770efc1cdaf16
FukaTamashi <vstepaniuk1@mail.ru>
2024-04-02 00:41:02 +0300

    feat: added header for Filefn class

---
+++ FAT32/FileLfn.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "File.h"
+
+class FileLfn : public File
+{
+private:
+	wstring getPartName(const UCHAR* record, int offsetStart, int offsetEnd);
+public:
+	FileLfn() {};
+	~FileLfn() {};
+
+	virtual void createFileName(const UCHAR* record);
+	virtual void createSize(const UCHAR* record);
+	virtual UINT32 createFirstCluster(const UCHAR* record);
+};
